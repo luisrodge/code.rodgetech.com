97090e7c2386a1ddc0d064f85deda51d5bf9c2ff
Luis Rodriguez <luisrodge@outlook.com>
2022-01-14 19:49:48 -0600

    Update header

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,12 +9,20 @@ const Layout = ({ location, title, children }) => {
   if (isRootPath) {
     header = (
       <h1 className="main-heading">
-        <Link to="/">{title}</Link>
+        <Link to="/">
+          <>
+            <span style={{ color: "#d1dce5", fontSize: "30px" }}>
+              rodgeTech
+            </span>
+            {title}
+          </>
+        </Link>
       </h1>
     )
   } else {
     header = (
       <Link className="header-link-home" to="/">
+        <span style={{ color: "#d1dce5", fontSize: "25px" }}>rodgeTech</span>
         {title}
       </Link>
     )
@@ -24,10 +32,14 @@ const Layout = ({ location, title, children }) => {
     <div className="global-wrapper" data-is-root-path={isRootPath}>
       <header className="global-header">{header}</header>
       <main>{children}</main>
+      <hr />
       <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+        <p>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.com">Gatsby</a> - Contact{" "}
+          <a href="mailto:rodgetech@gmail.com">rodgetech@gmail.com</a>
+        </p>
       </footer>
     </div>
   )
